7ca351b35705736f22d3206bbd6de663382b3916
wedojava <wedojava@gmail.com>
2020-01-25 22:53:44 +0800

    line number count.

--- Ch1/number_count.c
+++ Ch1/number_count.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 main(){
     long nc1;
-
     nc1 = 0;
     while (getchar() != EOF)
         ++nc1;
@@ -11,4 +10,11 @@ main(){
     for (nc2 = 0; getchar() != EOF; ++nc2)
         ;
     printf("nc2: %.0f\n", nc2);
+
+    int c, n1;
+    n1 = 0;
+    while ((c=getchar()) != EOF)
+        if (c == '\n')
+            ++n1;
+    printf("n1: %d\n", n1);
 }
