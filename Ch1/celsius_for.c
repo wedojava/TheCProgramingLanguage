4f220f0d7fd57b26ff1678195fdf66810096b463
wedojava <wedojava@gmail.com>
2020-01-25 20:43:50 +0800

    putchar v1

--- Ch1/celsius_for.c
+++ Ch1/celsius_for.c
@@ -4,10 +4,14 @@
 
 #include <stdio.h>
 
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
+
 main() {
     printf("%4s | %6s\n", "Fahr", "Celsius");
     printf("%4s-|-%6s\n", "----", "------");
-    for (int fahr = 0; fahr <= 301; fahr = fahr + 20) {
+    for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
         printf("%4d   %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
