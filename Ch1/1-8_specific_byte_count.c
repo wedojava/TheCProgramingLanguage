a329e0360d95cac13a14fad8459aa2a4bf85760d
wedojava <wedojava@gmail.com>
2020-01-26 16:25:28 +0800

    chapter 1 practice 1-9 complete.

--- Ch1/1-8_specific_byte_count.c
+++ Ch1/1-8_specific_byte_count.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-main()
-{
+
+main() {
     int c, nb, nt, nl;
     nb = 0;
     nt = 0;
     nl = 0;
-    while ((c = getchar()) != EOF)
-    {
+    while ((c = getchar()) != EOF) {
         if (c == (' '))
             nb++;
         else if (c == '\t')
---
+++ Ch1/1-9_iocopy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define NONBLANK 'a'
+main() {
+    int c,lastc;
+    lastc = NONBLANK;
+
+    while ((c = getchar()) != EOF){
+//  Method 1
+//        if (c != ' ')
+//            putchar(c);
+//        if (c == ' ')
+//            if (lastc != ' ')
+//                putchar(c);
+//        lastc = c;
+
+//  Method 2
+//        if (c != ' ')
+//            putchar(c);
+//        else if (lastc != ' ')
+//            putchar(c);
+//        lastc = c;
+//  Method 3
+        if (c != ' ' || lastc != ' ')
+            putchar(c);
+        lastc = c;
+    }
+}
